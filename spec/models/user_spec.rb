f3eb1b330ff0db10d53d98e06df7cbdaedd4c5b1
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-10-30 20:10:27 +0100

    refactor spec user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,11 +10,5 @@
 require 'spec_helper'
 
 describe User do
-  before do
-    @user = FactoryGirl.create :user
-  end
-
-  subject { @user }
-
   it { should have_many(:achievements).through(:subscriptions) }
 end
