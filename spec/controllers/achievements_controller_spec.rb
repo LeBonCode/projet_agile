8ac939bfb1ecb49389369fe4bb4a5b36d22c34ce
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-10-31 14:52:13 +0100

    show d'un achievement

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -6,11 +6,18 @@ describe AchievementsController do
     assigns[:achievement].should be_a_new(Achievement)
     response.should be_success
   end
+
   it 'should post create' do
     achievement = FactoryGirl.build(:achievement)
     post 'create', :achievement => { title: achievement.title, description: achievement.description }
     assigns(:achievement).should be_a_kind_of(Achievement)
     assigns(:achievement).should_not be_new_record
     response.should redirect_to new_achievement_path
+  end
+
+  it 'should get show' do
+    achievement = FactoryGirl.create(:achievement)
+    get 'show', id: achievement.id
+    assigns[:achievement].should eq(achievement)
   end
 end
