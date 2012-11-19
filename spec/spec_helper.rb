f68acc374489f15e8fb51bdda5e88a95960555ce
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-11-20 00:11:06 +0100

    guardfile + conf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,31 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'paperclip/matchers'
+require 'rubygems'
+require 'spork'
 
-RSpec.configure do |config|
-  config.include Paperclip::Shoulda::Matchers
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'paperclip/matchers'
 
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  Dir[Rails.root.join('spec/support/**/*.rb')].each{|f| require f}
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = 'random'
+  RSpec.configure do |config|
+    config.include Paperclip::Shoulda::Matchers
+    config.include FactoryGirl::Syntax::Methods
+
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.run_all_when_everything_filtered = true
+    config.filter_run :focus
+
+    # Run specs in random order to surface order dependencies. If you find an
+    # order dependency and want to debug it, you can fix the order by providing
+    # the seed, which is printed after each run.
+    #     --seed 1234
+    config.order = 'random'
+  end
+end
+
+Spork.each_run do
+  load "#{Rails.root}/config/routes.rb"
+  Dir["#{Rails.root}/app/**/*.rb"].each {|f| load f}
 end
