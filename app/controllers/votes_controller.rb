9164555ed0af25bafbd774c0f727de2ff28afb97
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-11-21 18:47:29 +0100

    vote create : second test

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,9 @@ class VotesController < ApplicationController
   def create
     subscription = Subscription.find(params[:vote][:subscription_id])
     @vote = Vote.create
-    flash[:notice] = 'Votre vote a bien été enregistré'
+    if !@vote.new_record? then
+      flash[:notice] = 'Votre vote a bien été enregistré'
+    end
     redirect_to subscription.user
   end
 end
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -3,11 +3,19 @@ require 'spec_helper'
 describe VotesController do
   let(:vote) { FactoryGirl.build(:vote) }
   context'#create' do
-    it "should redirect to user show with a notice on successful save" do
+    it 'should redirect to user show with a notice on successful save' do
       Vote.any_instance.stub(:valid?).and_return(true)
       post 'create', vote: { subscription_id: vote.subscription_id }
       assigns[:vote].should_not be_new_record
       flash[:notice].should_not be_nil
+      response.should redirect_to(user_path(vote.subscription.user_id))
+    end
+
+    it 'should re-render new template on failed save' do
+      Vote.any_instance.stub(:valid?).and_return(false)
+      post 'create', vote: { subscription_id: vote.subscription_id }
+      assigns[:vote].should be_new_record
+      flash[:notice].should be_nil
       response.should redirect_to(user_path(vote.subscription.user_id))
     end
   end
