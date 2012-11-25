47bb46a142d7167e27fe0bc1a8d3489e2c4b8780
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-11-25 19:44:26 +0100

    'Ma page' title for current user public page + fake current user removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
     @succeeded_subscriptions = User.find(params[:id]).subscriptions.succeeded
     @not_succeeded_subscriptions = User.find(params[:id]).subscriptions.not_succeeded
-    @fake_current_user = User.first
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe UsersController do
-  before do
-    @user = FactoryGirl.create(:user_with_subscriptions)
-  end
+  let(:user) {  FactoryGirl.create(:user_with_subscriptions) }
 
   it 'should get show' do
-    get 'show', id: @user.id
-    assigns[:succeeded_subscriptions].should eq(@user.subscriptions.succeeded)
-    assigns[:not_succeeded_subscriptions].should eq(@user.subscriptions.not_succeeded)
+    get 'show', id: user.id
+    assigns[:succeeded_subscriptions].should eq(user.subscriptions.succeeded)
+    assigns[:not_succeeded_subscriptions].should eq(user.subscriptions.not_succeeded)
+    assigns[:user].should eq(user)
   end
  end
