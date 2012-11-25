d84cb448b77778339cf919e026198e9bc05a56ef
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-11-25 13:42:35 +0100

    rspec conf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,12 +14,13 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'paperclip/matchers'
-
   Dir[Rails.root.join('spec/support/**/*.rb')].each{|f| require f}
 
   RSpec.configure do |config|
     config.include Paperclip::Shoulda::Matchers
     config.include FactoryGirl::Syntax::Methods
+
+    config.mock_with :rspec
 
     config.treat_symbols_as_metadata_keys_with_true_values = true
     config.run_all_when_everything_filtered = true
@@ -52,6 +53,5 @@ Spork.each_run do
     SimpleCov.start 'rails'
   end
 
-  load "#{Rails.root}/config/routes.rb"
-  Dir["#{Rails.root}/app/**/*.rb"].each {|f| load f}
+  FactoryGirl.reload
 end
